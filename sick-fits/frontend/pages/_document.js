11d0db2d8d5054500226f7a143b6f1705ed7520c
ashutoshvrm8 <ashutoshvrm8@gmail.com>
2019-03-14 16:52:52 +0530

    added info for _document.js

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,3 +1,6 @@
+// _document is only rendered on the server side and not on the client side
+// Event handlers like onClick can't be added to this file
+
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
